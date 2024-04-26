fb65bbd492d084c00a56f0da0f1e9a80977017c4
WanjikuN <cikunjoroge4@gmail.com>
2024-04-26 21:28:45 +0300

    refactor: Add navbar styling

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,8 +1,20 @@
 const LandingPage = () =>{
     return(
         <div id="landing">
-            Land
-            
+            {/* cont */}
+            <div id='nav'>
+                {/* nav */}
+                <h2><i>SnapSafari</i></h2>
+            </div>
+            <div id="landing-body">
+                {/* body cont */}
+                <div id="landing-content">
+                    {/* content */}
+                </div>
+                <div id="auth">
+                    {/* auth */}
+                </div>
+            </div>
         </div>
     )
 }
