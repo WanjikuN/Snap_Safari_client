7bce3464dba1c9e6a909318b05f6b465f18e599a
WanjikuN <cikunjoroge4@gmail.com>
2024-05-01 21:04:55 +0300

    feat: Update photos display

--- src/components/HomePage/OpenSidenav.jsx
+++ src/components/HomePage/OpenSidenav.jsx
@@ -1,7 +1,29 @@
-import Sidebar from "./Sidebar";
+import React, { useState, useEffect } from 'react';
 import { useLocation } from "react-router-dom";
 const OpenSidenav = ({selectedRow, onClose}) =>{
     const location = useLocation();
+    const [photos, setPhotos] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+   
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://snap-safari-backend.onrender.com/photos/');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setPhotos(data); 
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+  
+      fetchData();
+    }, []);
+    console.log(photos)
     return (
         <>
         <div className="side-navigation">
@@ -17,7 +39,21 @@ const OpenSidenav = ({selectedRow, onClose}) =>{
           :
           <>
           {/* Albums details */}
-           <p>Title: {selectedRow.title}</p>
+          <div id="photos-container">
+          {
+            photos.map(photo =>(
+                <div key={photo.id}  className='photos'>
+                   <div className="image-container">
+                    <img src={photo.image_url} alt="" />
+                <p className="view-button">View</p>
+                    </div>
+                    {/* <p>{photo.title}</p> */}
+
+                </div>
+            ))
+           }
+          </div>
+         
           </>}
         </div>
         </>
