fd86a112fe9c868fc520c6d35e041c62ac82c17e
WanjikuN <cikunjoroge4@gmail.com>
2024-05-01 13:55:57 +0300

    feat: Add routing path to icons

--- src/components/HomePage/Sidebar.jsx
+++ src/components/HomePage/Sidebar.jsx
@@ -1,10 +1,18 @@
-
-
+import { FaUsers } from "react-icons/fa";
+import { MdPhotoAlbum } from "react-icons/md";
+import { Link } from "react-router-dom";
 const Sidebar = () =>{
     return (
         <div id='sidebar'>
             {/* sidebar */}
-           
+            <div>
+            <Link to='/users'><FaUsers size={30} color="black"/></Link>
+
+            </div>
+            <div>
+            <Link to='/albums'><MdPhotoAlbum size={30} color="black" /></Link>
+
+            </div>
         </div>
     )
 }
