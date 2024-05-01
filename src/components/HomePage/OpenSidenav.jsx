c7deecfe25874d0ab80222896535b23ef279d5a1
WanjikuN <cikunjoroge4@gmail.com>
2024-05-01 19:38:19 +0300

    feat: Update content rendering

--- src/components/HomePage/OpenSidenav.jsx
+++ src/components/HomePage/OpenSidenav.jsx
@@ -1,13 +1,24 @@
 import Sidebar from "./Sidebar";
-
+import { useLocation } from "react-router-dom";
 const OpenSidenav = ({selectedRow, onClose}) =>{
+    const location = useLocation();
     return (
         <>
         <div className="side-navigation">
           <button onClick={onClose}>Close</button>
-          <p>Name: {selectedRow.name}</p>
+            {location.pathname === "/users"?
+            <>
+            {/* users details */}
+            <p>Name: {selectedRow.name}</p>
           <p>Username: {selectedRow.username}</p>
           <p>Email: {selectedRow.email}</p>
+            </>
+         
+          :
+          <>
+          {/* Albums details */}
+           <p>Title: {selectedRow.title}</p>
+          </>}
         </div>
         </>
         
