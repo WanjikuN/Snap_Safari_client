d0a064c82eb9b84118ba23c075ab944572c0f75c
WanjikuN <cikunjoroge4@gmail.com>
2024-05-01 17:18:18 +0300

    feat: Update column data

--- src/components/HomePage/Users.jsx
+++ src/components/HomePage/Users.jsx
@@ -12,9 +12,9 @@ console.log(rows)
       const fetchData = async () => {
           try {
               const response = await fetch('https://snap-safari-backend.onrender.com/users/');
-              // if (!response.ok) {
-              //     throw new Error('Network response was not ok');
-              // }
+              if (!response.ok) {
+                  throw new Error('Network response was not ok');
+              }
               console.log(response)
               const data = await response.json();
               setRows(data); 
@@ -37,7 +37,10 @@ console.log(rows)
 
   // Define your columns here
   const columns = [
-    // Add your column definitions here
+    { field: 'name', headerName: 'Name', width: 150 ,flex: 1 },
+    { field: 'username', headerName: 'Username', width: 150 ,flex: 1  },
+    { field: 'email', headerName: 'E-mail', width: 150,flex: 1  },
+    // Add more columns as needed
   ];
 
   // Define the function to handle edit cell changes
@@ -67,10 +70,11 @@ console.log(rows)
               pageSizeOptions={[15, 25, 50, 100]}
               className="datagrid-root"
               onRowClick={handleRowClick}
-              checkboxSelection
               disableColumnSelector
-              disableColumnFilter
-              getRowClassName={(params) => (params.indexRelativeToCurrentPage % 2 !== 0 ? 'striped-row' : {})}
+              getRowClassName=
+              {
+                (params) => (params.indexRelativeToCurrentPage % 2 !== 0 ? 'striped-row' : 'striped-row-none')
+              }
             />
           
       </div>
