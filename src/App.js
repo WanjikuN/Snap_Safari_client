7060cbabc85d4cd63932e636fabfb30747373666
WanjikuN <cikunjoroge4@gmail.com>
2024-05-01 10:44:35 +0300

    feat: Add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import { Routes, Route, useLocation } from "react-router-dom";
+
 import './assets/css/main.css';
 import LandingPage from './components/LandingPage/LandingPage';
+import Navbar from './components/Navbar/Navbar';
+import Users from './components/HomePage/Users';
 
 function App() {
   return (
     <>
-    <LandingPage />
+   <Navbar />
+   <Routes>
+    <Route exact path="/" element={<LandingPage />} />
+    <Route exact path="/users" element={<Users />} />
+    </Routes>
+    
     </>
   );
 }
