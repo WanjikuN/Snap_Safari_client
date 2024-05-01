cf1409bc956dab0c4e0a1c084dc510c59c41239d
WanjikuN <cikunjoroge4@gmail.com>
2024-05-01 14:06:25 +0300

    Update albums route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './assets/css/main.css';
 import LandingPage from './components/LandingPage/LandingPage';
 import Navbar from './components/Navbar/Navbar';
 import Users from './components/HomePage/Users';
+import Albums from './components/HomePage/Albums';
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
    <Routes>
     <Route exact path="/" element={<LandingPage />} />
     <Route exact path="/users" element={<Users />} />
+    <Route exact path="/albums" element={<Albums />} />
+
     </Routes>
     
     </>
